aeba486e2f8b3a27bbcf4e7dac19bdd655e24d30
Rosivania <rosivania.soaresdasilva@gmail.com>
2019-08-07 23:00:59 -0300

    Adicionando o roteamento de páginas

---
+++ aquecimento-react/src/components/Sobre.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sobre extends Component {
+    render() {
+        return (
+            <div>
+                <div>Sobre</div>
+            </div>
+        );
+    }
+}
+
+export default Sobre;
