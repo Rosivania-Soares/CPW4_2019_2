94fa355b02bfd108e65c6a5e53d1a8bb7e835dd6
Rosivania <rosivania.soaresdasilva@gmail.com>
2019-08-22 21:11:02 -0300

    Carregando as listas na página inicial

--- lista-compras/src/components/lista/Listas.js
+++ lista-compras/src/components/lista/Listas.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import Menu from '../menu/Menu';
 
+import Menu from '../menu/Menu';
 import logo from '../../img/logo.png';
+import ListaService from '../../services/ListaService';
 
 class Listas extends Component {
+    constructor() {
+        super();
+        this.state = {
+            listas: []
+        }
+        this.service = new ListaService();
+    }
+
+    componentDidMount() {
+        const listas = this.service.recuperarListas();
+        this.setState({ listas });
+    }
     render() {
+        const listas = this.state.listas.map(lista => (
+            <li key={lista._id}>{lista.nome}</li>
+        ));
+
         return (
             <div>
-                <Menu logo={logo} paginaAnterior="/" titulo ="Lista de Compras"/>
-                Listas
+                <Menu logo={logo} paginaAnterior="/" titulo="Lista de Compras" />
+               
+                <div className="conteiner">
+                    <div>
+                        <h2>Minhas listas</h2>
+                        <ul>
+                            {listas}
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     }
