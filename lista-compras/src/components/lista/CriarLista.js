0eb3e08f91cf51cbd2e3a7dba690c4204128668f
Rosivania <rosivania.soaresdasilva@gmail.com>
2019-09-04 22:42:00 -0300

    Aula dia 04 de setembro

--- lista-compras/src/components/lista/CriarLista.js
+++ lista-compras/src/components/lista/CriarLista.js
@@ -2,18 +2,40 @@ import React, { Component } from 'react'
 
 import Menu from '../menu/Menu';
 import icone from '../../img/paginaAnterior.png';
+import ListaService from '../../services/ListaService';
 
 export default class CriarLista extends Component {
     state = {
-        nome : ''
+        nome: ''
     };
+
+    aoAlterarNome = (event) => {
+        const nome = event.target.value;
+        this.setState({ nome });
+    }
+    salvar = (event)=>{
+        /**
+         * Previne o comportamento padrão
+         * do formulário, que é recarregar a página.
+         */
+        event.preventDefault();
+        const service = new ListaService();
+        const lista = this.state;
+        service.salvar(lista);
+
+        /**
+         * Faz o encaminhamento para a
+         * página inicial doapp.
+         */
+        this.props.history.push('/');
+    }
 
     render() {
         return (
             <div>
                 <Menu paginaAnterior="/" titulo="Criar lista" logo={icone} />
-                <form id="formNovaLista">
-                    <input type="text" name="nome" value={this.state.nome} required placeholder="Digite o nome da lista" />
+                <form id="formNovaLista" onSubmit={this.salvar}>
+                    <input onChange={this.aoAlterarNome} type="text" name="nome" value={this.state.nome} required placeholder="Digite o nome da lista" />
                     <input type="submit" value="Criar" />
                 </form>
             </div>
--- lista-compras/src/services/ListaService.js
+++ lista-compras/src/services/ListaService.js
@@ -5,15 +5,19 @@ export default class ListaService {
     constructor() {
         this.listas = [];
         this.api = axios.create({
-            baseURL:"https:\\secret-plateau-95576.herokuapp.com"
+            baseURL: "https:\\secret-plateau-95576.herokuapp.com/listas"
         });
     }
 
     async recuperarListas() {
-        let resposta =await this.api.get('/listas');
-        this.listas =resposta.data;
+        let resposta = await this.api.get('/');
+        this.listas = resposta.data;
         return this.listas;
-         
+
+    }
+
+    async salvar(lista) {
+        await this.api.post('/', lista);
     }
 
 }
